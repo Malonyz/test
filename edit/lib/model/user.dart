721457198136ecf4296d21a40da2ee123923d32c
Smug_M <mapmap1234@hotmail.com>
2022-01-26 17:09:19 +0700

    test

---
+++ edit/lib/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final String imagePath;
+  final String name;
+  final String age;
+  final String about;
+  final bool isDarkMode;
+
+  const User({
+    required this.imagePath,
+    required this.name,
+    required this.age,
+    required this.about,
+    required this.isDarkMode,
+  });
+}
---
+++ edit/lib/utils/user_preferences.dart
@@ -0,0 +1,12 @@
+import 'package:edit/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://static.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png/revision/latest?cb=20150206140125',
+    name: 'YODA กระเจี๊ยวเขียว',
+    age: '20',
+    about: 'a fictional character in the Star Wars universe.',
+    isDarkMode: false,
+  );
+}
